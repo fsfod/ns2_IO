ab7af44a61eaaff2a00c22050d59c3cc6efafea0
fsfod <z>
2011-12-10 11:20:05 +0000

    Fix name mangling of Luabinds module entry export

--- LuabindModule.cpp
+++ LuabindModule.cpp
@@ -1,8 +1,11 @@
 
 #include "lua.h"
-#include "luabind/open.hpp"
 
-__declspec(dllexport) int __cdecl luabind_open(lua_State* L){
+namespace luabind {
+   void open(lua_State* L);
+}
+
+extern "C" __declspec(dllexport) int __cdecl luaopen_luabind(lua_State* L){
 
   luabind::open(L);
 
